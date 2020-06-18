a01c6c754fc7a1aff4528e29ef4f27da15479bc8
ddunn40495 <ddunn40495@gmail.com>
2020-06-18 18:29:19 -0400

    added some CSS, added button in HTML, made event handler for button

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 // create basic CSS style
 // Create JS elements
 $(()=> {
-    var settings = {
+    const settings = {
         "async": true,
         "crossDomain": true,
         "url": "https://tsa-wait-times.p.rapidapi.com/airports/test?APIKEY=test",
@@ -13,8 +13,29 @@ $(()=> {
             "x-rapidapi-key": "5a2ec78370msh595bb78bc5f9287p1c3ec1jsn60e709003213"
         }
     }
-    
-    
+
+
+   
+    $('#main-button').click(function() { 
+        const airportVal = $('#search-box').val()
+       
+    })
+    const getTimes = () => {
+        $.ajax({
+          url: settings.url,
+          headers: settings.headers
+        }).then((data) => {
+          $('#waittime_headine').html(`
+          <h3> ${getTimes.city} </h3>
+          <h3> ${getTimes.code} </h3>
+          <h3> ${getTimes.state} </h3>
+          `)
+        
+          
+        })
+      }
+      
+      getTimes()
 
 
 
