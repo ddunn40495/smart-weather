85dff98b78a8bde38c80b372ab5f14d50151779a
ddunn40495 <ddunn40495@gmail.com>
2020-06-18 15:12:03 -0400

    Added API Code/Key in JS file

---
+++ app.js
@@ -0,0 +1,22 @@
+// create HTML CSS JS File
+// create HTML structure
+// create basic CSS style
+// Create JS elements
+$(()=> {
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://tsa-wait-times.p.rapidapi.com/airports/test?APIKEY=test",
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "tsa-wait-times.p.rapidapi.com",
+            "x-rapidapi-key": "5a2ec78370msh595bb78bc5f9287p1c3ec1jsn60e709003213"
+        }
+    }
+    
+    
+
+
+
+
+})
