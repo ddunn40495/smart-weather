49befc1d026a04d2c184c3278dc14c21014f88a2
ddunn40495 <ddunn40495@gmail.com>
2020-06-19 17:40:17 -0400

    got event listener to grab content from search box, and to insert that value in AJAX call, AJAX call and event listener is working

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 $( () => {
 
-$('#search-button').click(function() { 
-       const locationVal = $('#search-box').val()
-      
- })
+      // $('').click(function() { 
+      //       const locationVal = $('#search-box').val()
+      //      console.log(locationVal)
+      // })
+      $('#search-button').on('click', (event) => {
+            event.preventDefault()
+            currCityCode = $('#search-box').val()
+            getForecast()
+
+          })
 
 const baseURL = `http://api.weatherapi.com/v1/current.json?key=`
 const apiKey = `7c70246dd148451ab10161808201906`
@@ -17,17 +23,27 @@ const getForecast = () => {
         url: queryURL + currCityCode
       }).then((weathData) => {
             // console.log(weathData)
-      $(".forecast-results").html(`
-      <h2> ${weathData.location} </h2>
-      <h3> ${weathData.current} </h3>
+      $('.forecast-results').html(`
+      <h2> ${weathData.location.name} </h2>
+      <h3> ${weathData.location.region}   ${weathData.location.country} </h3>
+      <h3> ${weathData.current.temp_f} F</h3>
+      <h3> ${weathData.current.condition.text} </h3>
+      <h3> Precipitation ${weathData.current.precip_in} inches </h3>
+      <h3> Humidity:  ${weathData.current.humidity} </h3>
+      <h3> Feels like:  ${weathData.current.feelslike_f} </h3>
+      <h3> Wind: ${weathData.current.wind_mph} </h3>
       `)
+      const $img = $('<img>').attr('src', weathData.current.condition.icon).attr('alt', weathData.current.condition.text)
+      $('.forecast-results').append($img)
       }, (error) => {
         console.error(error)
       })
     }
       
-    getForecast()
+    
 
+
+ 
 
 
 
