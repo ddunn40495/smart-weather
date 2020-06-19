f9363369060d7fedea8c56148c17cd055d80372d
ddunn40495 <ddunn40495@gmail.com>
2020-06-19 10:38:00 -0400

    Working on new API settings

--- app.js
+++ app.js
@@ -3,16 +3,25 @@
 // create basic CSS style
 // Create JS elements
 $(()=> {
-    const settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": "https://tsa-wait-times.p.rapidapi.com/airports/test?APIKEY=test",
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "tsa-wait-times.p.rapidapi.com",
-            "x-rapidapi-key": "5a2ec78370msh595bb78bc5f9287p1c3ec1jsn60e709003213"
-        }
-    }
+    // const settings = {
+    //     "async": true,
+    //     "crossDomain": true,
+    //     "url": "https://tsa-wait-times.p.rapidapi.com/airports/test?APIKEY=test",
+    //     "method": "GET",
+    //     "headers": {
+    //         "x-rapidapi-host": "tsa-wait-times.p.rapidapi.com",
+    //         "x-rapidapi-key": "5a2ec78370msh595bb78bc5f9287p1c3ec1jsn60e709003213"
+    //     }
+    // }
+
+    const baseURL = `https://www.tsawaittimes.com/`
+    const apiKey = `uxXWAUUO0PGFyeyYn7btpURfmhQnr8Mf`
+    const queryType = `t=`
+    let titleQuery = 'eraserhead'
+    let queryURL = baseURL + apiKey + '&' + queryType + titleQuery
+
+
+
 
 
    
