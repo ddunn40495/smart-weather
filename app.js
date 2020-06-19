0550d91e00be9720abb5f2d3bb914dc142f1688b
ddunn40495 <ddunn40495@gmail.com>
2020-06-19 14:52:48 -0400

    Change HMTL, classes, ids, to fit with new API. Got weather APIand AJAX call running

--- app.js
+++ app.js
@@ -1,54 +1,40 @@
-// create HTML CSS JS File
-// create HTML structure
-// create basic CSS style
-// Create JS elements
-$(()=> {
-    // const settings = {
-    //     "async": true,
-    //     "crossDomain": true,
-    //     "url": "https://tsa-wait-times.p.rapidapi.com/airports/test?APIKEY=test",
-    //     "method": "GET",
-    //     "headers": {
-    //         "x-rapidapi-host": "tsa-wait-times.p.rapidapi.com",
-    //         "x-rapidapi-key": "5a2ec78370msh595bb78bc5f9287p1c3ec1jsn60e709003213"
-    //     }
-    // }
+$( () => {
 
-    const baseURL = `https://www.tsawaittimes.com/api/airports/`
-    const apiKey = `uxXWAUUO0PGFyeyYn7btpURfmhQnr8Mf/`
-    // const queryType = `t=`
-    const format = JSON
-    // let airCode = 'ATL'
-    let airportsListURL = baseURL + apiKey + format 
-
-  console.log(airportsListURL)
-
-
-
-   
-    $('#main-button').click(function() { 
-        const airportVal = $('#search-box').val()
-       console.log(airportVal)
-       getTimes(airportVal)
-    })
-    const getTimes = () => {
-        $.ajax({
-          url: airportsListURL
-        }).then((data) => {
-          console.log(data)
-          $('#waittime_headine').html(`
-          <h3> ${getTimes.city} </h3>
-          <h3> ${getTimes.code} </h3>
-          <h3> ${getTimes.state} </h3>
-          `)
-        
-          
-        })
-      }
+$('#search-button').click(function() { 
+       const locationVal = $('#search-box').val()
       
+ })
+
+const baseURL = `http://api.weatherapi.com/v1/current.json?key=`
+const apiKey = `7c70246dd148451ab10161808201906`
+let currCityCode = 30039
+const extraCode = `&q=`
+const queryURL = baseURL + apiKey + extraCode 
+// console.log(queryURL)
+
+const getForecast = () => {
+      $.ajax({
+        url: queryURL + currCityCode
+      }).then((weathData) => {
+            // console.log(weathData)
+      $(".forecast-results").html(`
+      <h2> ${weathData.location} </h2>
+      <h3> ${weathData.current} </h3>
+      `)
+      }, (error) => {
+        console.error(error)
+      })
+    }
+      
+    getForecast()
+
+
+
+
+});
+
+
+
       
 
 
-
-
-})
